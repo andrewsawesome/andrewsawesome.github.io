dafb276461b27837ad819de8d54bb93099848968
andrewsawesome <36270171+andrewsawesome@users.noreply.github.com>
2019-05-26 20:13:00 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var endpoint = "https://www.jsonstore.io/aec0115772482375953d41e0f6c0a280d281ed2132e355d845142ea8aa55dffd"
+
+function getrandom(){
+var random_string =
+Math.random().toString(32).substring(2, 5) +
+Math.random().toString(32).substring(2, 5):
+return random_string()
+}
