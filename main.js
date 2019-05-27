f2fc12547ebdd27e863888959886dd3ecd2f0bdf
andrewsawesome <36270171+andrewsawesome@users.noreply.github.com>
2019-05-26 20:17:32 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+#This is the JavaScript file for my Url Shortener#
 var endpoint = "https://www.jsonstore.io/aec0115772482375953d41e0f6c0a280d281ed2132e355d845142ea8aa55dffd"
 
 function getrandom(){
